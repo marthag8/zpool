6faa28aeb29bc9049e1e4e7eee1b7806e4076b8c
Martha Greenberg <marthag@techfinancials.com>
2014-12-14 17:21:43 +0200

    add name to metadata, move to new account, version bump

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "zpool"
 maintainer       "Martha Greenberg"
-maintainer_email "marthag@wix.com"
+maintainer_email "marthag@mit.edu"
 license          "Apache 2.0"
 description      "Manages Solaris zpools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 %w[solaris2 ubuntu].each do |os|
   supports os
