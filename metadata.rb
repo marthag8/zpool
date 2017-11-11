fddea1f7d81c77c5127a72063d7fe93069b1f499
Ryan Hass <rhass@users.noreply.github.com>
2017-11-10 20:09:18 -0800

    Creake chef_zpool cookbook fork.

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             'zpool'
-maintainer       'Martha Greenberg'
-maintainer_email 'marthag@mit.edu'
-license          'Apache-2.0'
-description      'Manages ZFS zpools'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-source_url 'https://github.com/marthag8/zpool'
-issues_url 'https://github.com/marthag8/zpool/issues'
+name              'chef_zpool'
+maintainer        'Chef Software Inc.
+maintainer_email  'cookbooks@chef.io'
+license           'Apache-2.0'
+description       'Manages ZFS zpools'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
+source_url        'https://github.com/chef-cookbooks/chef_zpool'
+issues_url        'https://github.com/marthag8/zpool/issues'
 
 %w(solaris2 ubuntu freebsd).each do |os|
   supports os
